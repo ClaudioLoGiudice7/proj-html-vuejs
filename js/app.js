89d5208a8c7227225ca626867acedf6ced2a886c
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-02-22 02:16:51 +0100

    entering structure fixes, every little detail and header links made with vue

--- js/app.js
+++ js/app.js
@@ -9,3 +9,58 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+        document.getElementById("scroll-top").style.display = "block";
+    } else {
+        document.getElementById("scroll-top").style.display = "none";
+    }
+}
+
+const app = Vue.createApp({
+    data() {
+        return {
+            links: [
+                {
+                    text: "Home",
+                    href: "#",
+                    class: "fs-5 yellow jump"
+                },
+                {
+                    text: "About Us",
+                    href: "#",
+                    class: "fs-5 jump"
+                },
+                {
+                    text: "Services",
+                    href: "#",
+                    class: "fs-5 jump"
+                },
+                {
+                    text: "Shop",
+                    href: "#",
+                    class: "fs-5 jump"
+                },
+                {
+                    text: "Our Team",
+                    href: "#",
+                    class: "fs-5 jump"
+                },
+                {
+                    text: "Blog",
+                    href: "#",
+                    class: "fs-5 jump"
+                },
+                {
+                    text: "Contact Us",
+                    href: "#",
+                    class: "fs-5 jump"
+                },
+            ]
+        };
+    },
+});
+app.mount("#app");
